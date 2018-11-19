6a1fb2926916355d5500a6d51094543c8e3b5b1b
Jeffrey Huang <jeffreyhuang0331@gmail.com>
2018-11-19 16:43:08 +0800

    Add routee parameters

---
+++ components/Post.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="post">
+    <h1>{{ post.title }}</h1>
+
+    <p>{{ post.body }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    post: Object
+  }
+}
+</script>
